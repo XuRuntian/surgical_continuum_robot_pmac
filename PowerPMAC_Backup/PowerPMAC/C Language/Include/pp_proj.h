2640685b51bc58f58daec0cbe1b65ff24ab70e23
XuRuntian <xuruntian03@163.com>
2026-05-26 13:58:08 +0800

    update vresion

---
+++ PowerPMAC_Backup/PowerPMAC/C Language/Include/pp_proj.h
@@ -0,0 +1,342 @@
+
+#ifndef _PP_PROJ_H_
+#define _PP_PROJ_H_
+//***********************************************************************************
+// C header for accessing PMAC Global, CSGlobal, Ptr vars
+// _PPScriptMode_ for Pmac Script like access global & csglobal
+// global Mypvar - access with "Mypvar"
+// global Myparray(32) - access with "Myparray(i)"
+// csglobal Myqvar - access with "Myqvar(i)" where "i" is Coord #
+// csglobal Myqarray(16) - access with "Myqvar(i,j)" where "j" is index
+// _EnumMode_ for Pmac enum data type checking on Set & Get global functions
+// Example
+// global Mypvar
+// csglobal Myqvar
+// "SetGlobalVar(Myqvar, data)" will give a compile error because its a csglobal var.
+// "SetCSGlobalVar(Mypvar, data)" will give a compile error because its a global var.
+//************************************************************************************
+
+#ifdef _PPScriptMode_
+enum globalP {_globalP_=-1};
+enum globalParray {_globalParray_=-1};
+enum csglobalQ {_csglobalQ_=-1};
+enum csglobalQarray {_csglobalQarray_=-1};
+
+enum ptrM {_ptrM_=-1};
+enum ptrMarray {_ptrMarray_=-1};
+#define	PVT_BufferSize	pshm->P[8192]
+#define	PVT_WriteIdx	pshm->P[8193]
+#define	PVT_ReadIdx	pshm->P[8194]
+#define	PVT_Count	pshm->P[8195]
+#define	PVT_Scale	pshm->P[8196]
+#define	PVT_Time(i)	pshm->P[(8197+i)%MAX_P]
+#define	PVT_Pos(i)	pshm->P[(8297+i)%MAX_P]
+#define	PVT_Vel(i)	pshm->P[(8897+i)%MAX_P]
+#define	target_pos1	pshm->P[9497]
+#define	target_pos2	pshm->P[9498]
+#define	target_pos3	pshm->P[9499]
+#define	target_pos4	pshm->P[9500]
+#define	target_pos5	pshm->P[9501]
+#define	target_move_time	pshm->P[9502]
+#define	target_accel_time	pshm->P[9503]
+#define	target_scurve_time	pshm->P[9504]
+#define	MyInit_Flag	pshm->P[9505]
+#define	IsNewData	pshm->P[9506]
+#define	base	pshm->P[9507]
+#define	Homing_State	pshm->P[9508]
+#define	Homing_Done	pshm->P[9509]
+#define	Homing_Timer	pshm->P[9510]
+#define	max_length	pshm->P[9511]
+#define	Last_Act_Pos	pshm->P[9512]
+#define	Last_Iq_Cmd	pshm->P[9513]
+#define	threshold_iqcmd	pshm->P[9514]
+#define	fe_max	pshm->P[9515]
+#define	Homing_Stop_Reason	pshm->P[9516]
+#define	error	pshm->P[9517]
+#ifndef _PP_PROJ_HDR_
+  void SetEnumGlobalVar(enum globalP var, double data)
+  {
+    pshm->P[var] = data;
+  }
+
+  double GetEnumGlobalVar(enum globalP var)
+  {
+    return pshm->P[var];
+  }
+
+  void SetEnumGlobalArrayVar(enum globalParray var, unsigned index, double data)
+  {
+    pshm->P[(var + index)%MAX_P] = data;
+  }
+
+  double GetEnumGlobalArrayVar(enum globalParray var, unsigned index)
+  {
+    return pshm->P[(var + index)%MAX_P];
+  }
+
+  void SetEnumCSGlobalVar(enum csglobalQ var, unsigned cs, double data)
+  {
+    pshm->Coord[cs % MAX_COORDS].Q[var] = data;
+  }
+
+  double GetEnumCSGlobalVar(enum csglobalQ var, unsigned cs)
+  {
+    return pshm->Coord[cs % MAX_COORDS].Q[var];
+  }
+
+  void SetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs, double data)
+  {
+    pshm->Coord[cs % MAX_COORDS].Q[(var + index)%MAX_Q] = data;
+  }
+
+  double GetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs)
+  {
+    return pshm->Coord[cs % MAX_COORDS].Q[(var + index)%MAX_Q];
+  }
+
+  void SetEnumPtrVar(enum ptrM var, double data)
+  {
+    im_write(pshm->Mdef + var, data, &pshm->Ldata);
+  }
+
+  double GetEnumPtrVar(enum ptrM var)
+  {
+    return im_read(pshm->Mdef + var, &pshm->Ldata);
+  }
+
+  void SetEnumPtrArrayVar(enum ptrMarray var, unsigned index, double data)
+  {
+    im_write(pshm->Mdef + ((var + index)%MAX_M), data, &pshm->Ldata);
+  }
+
+  double GetEnumPtrArrayVar(enum ptrMarray var, unsigned index)
+  {
+    return im_read(pshm->Mdef + ((var + index)%MAX_M), &pshm->Ldata);
+  }
+
+  #define SetGlobalVar(i, x)              SetEnumGlobalVar(i, x)
+  #define SetGlobalArrayVar(i, j, x)      SetEnumGlobalArrayVar(i, j, x)
+  #define GetGlobalVar(i)                 GetEnumGlobalVar(i)
+  #define GetGlobalArrayVar(i, j)         GetEnumGlobalArrayVar(i, j)
+
+  #define SetCSGlobalVar(i, j, x)         SetEnumCSGlobalVar(i, j, x)
+  #define SetCSGlobalArrayVar(i, j, k, x) SetEnumCSGlobalArrayVar(i, j, k, x)
+  #define GetCSGlobalVar(i, j)            GetEnumCSGlobalVar(i, j)
+  #define GetCSGlobalArrayVar(i, j, k)    GetEnumCSGlobalArrayVar(i, j, k)
+
+  #define SetPtrVar(i, x)                 SetEnumPtrVar(i, x)
+  #define SetPtrArrayVar(i, j, x)         SetEnumPtrArrayVar(i, j, x)
+  #define GetPtrVar(i)                    GetEnumPtrVar(i)
+  #define GetPtrArrayVar(i, j)            GetEnumPtrArrayVar(i, j)
+
+#else
+
+  void SetEnumGlobalVar(enum globalP var, double data);
+  double GetEnumGlobalVar(enum globalP var);
+  void SetEnumGlobalArrayVar(enum globalParray var, unsigned index, double data);
+  double GetEnumGlobalArrayVar(enum globalParray var, unsigned index);
+  void SetEnumCSGlobalVar(enum csglobalQ var, unsigned cs, double data);
+  double GetEnumCSGlobalVar(enum csglobalQ var, unsigned cs);
+  void SetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs, double data);
+  double GetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs);
+  void SetEnumPtrVar(enum ptrM var, double data);
+  double GetEnumPtrVar(enum ptrM var);
+  void SetEnumPtrArrayVar(enum ptrMarray var, unsigned index, double data);
+  double GetEnumPtrArrayVar(enum ptrMarray var, unsigned index);
+
+  #define SetGlobalVar(i, x)              SetEnumGlobalVar(i, x)
+  #define SetGlobalArrayVar(i, j, x)      SetEnumGlobalArrayVar(i, j, x)
+  #define GetGlobalVar(i)                 GetEnumGlobalVar(i)
+  #define GetGlobalArrayVar(i, j)         GetEnumGlobalArrayVar(i, j)
+
+  #define SetCSGlobalVar(i, j, x)         SetEnumCSGlobalVar(i, j, x)
+  #define SetCSGlobalArrayVar(i, j, k, x) SetEnumCSGlobalArrayVar(i, j, k, x)
+  #define GetCSGlobalVar(i, j)            GetEnumCSGlobalVar(i, j)
+  #define GetCSGlobalArrayVar(i, j, k)    GetEnumCSGlobalArrayVar(i, j, k)
+
+  #define SetPtrVar(i, x)                 SetEnumPtrVar(i, x)
+  #define SetPtrArrayVar(i, j, x)         SetEnumPtrArrayVar(i, j, x)
+  #define GetPtrVar(i)                    GetEnumPtrVar(i)
+  #define GetPtrArrayVar(i, j)            GetEnumPtrArrayVar(i, j)
+
+#endif
+// end of #ifdef _PPScriptMode_
+#else
+#ifdef _EnumMode_
+enum globalP {_globalP_=-1
+,PVT_BufferSize=8192
+,PVT_WriteIdx=8193
+,PVT_ReadIdx=8194
+,PVT_Count=8195
+,PVT_Scale=8196
+,target_pos1=9497
+,target_pos2=9498
+,target_pos3=9499
+,target_pos4=9500
+,target_pos5=9501
+,target_move_time=9502
+,target_accel_time=9503
+,target_scurve_time=9504
+,MyInit_Flag=9505
+,IsNewData=9506
+,base=9507
+,Homing_State=9508
+,Homing_Done=9509
+,Homing_Timer=9510
+,max_length=9511
+,Last_Act_Pos=9512
+,Last_Iq_Cmd=9513
+,threshold_iqcmd=9514
+,fe_max=9515
+,Homing_Stop_Reason=9516
+,error=9517};
+enum globalParray {_globalParray_=-1
+,PVT_Time=8197
+,PVT_Pos=8297
+,PVT_Vel=8897};
+enum csglobalQ {_csglobalQ_=-1};
+enum csglobalQarray {_csglobalQarray_=-1};
+enum ptrM {_ptrM_=-1};
+enum ptrMarray {_ptrMarray_=-1};
+#ifndef _PP_PROJ_HDR_
+  void SetEnumGlobalVar(enum globalP var, double data)
+  {
+    pshm->P[var] = data;
+  }
+
+  double GetEnumGlobalVar(enum globalP var)
+  {
+    return pshm->P[var];
+  }
+
+  void SetEnumGlobalArrayVar(enum globalParray var, unsigned index, double data)
+  {
+    pshm->P[(var + index)%MAX_P] = data;
+  }
+
+  double GetEnumGlobalArrayVar(enum globalParray var, unsigned index)
+  {
+    return pshm->P[(var + index)%MAX_P];
+  }
+
+  void SetEnumCSGlobalVar(enum csglobalQ var, unsigned cs, double data)
+  {
+    pshm->Coord[cs % MAX_COORDS].Q[var] = data;
+  }
+
+  double GetEnumCSGlobalVar(enum csglobalQ var, unsigned cs)
+  {
+    return pshm->Coord[cs % MAX_COORDS].Q[var];
+  }
+
+  void SetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs, double data)
+  {
+    pshm->Coord[cs % MAX_COORDS].Q[(var + index)%MAX_Q] = data;
+  }
+
+  double GetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs)
+  {
+    return pshm->Coord[cs % MAX_COORDS].Q[(var + index)%MAX_Q];
+  }
+
+  void SetEnumPtrVar(enum ptrM var, double data)
+  {
+    im_write(pshm->Mdef + var, data, &pshm->Ldata);
+  }
+
+  double GetEnumPtrVar(enum ptrM var)
+  {
+    return im_read(pshm->Mdef + var, &pshm->Ldata);
+  }
+
+  void SetEnumPtrArrayVar(enum ptrMarray var, unsigned index, double data)
+  {
+    im_write(pshm->Mdef + ((var + index)%MAX_M), data, &pshm->Ldata);
+  }
+
+  double GetEnumPtrArrayVar(enum ptrMarray var, unsigned index)
+  {
+    return im_read(pshm->Mdef + ((var + index)%MAX_M), &pshm->Ldata);
+  }
+
+  #define SetGlobalVar(i, x)              SetEnumGlobalVar(i, x)
+  #define SetGlobalArrayVar(i, j, x)      SetEnumGlobalArrayVar(i, j, x)
+  #define GetGlobalVar(i)                 GetEnumGlobalVar(i)
+  #define GetGlobalArrayVar(i, j)         GetEnumGlobalArrayVar(i, j)
+
+  #define SetCSGlobalVar(i, j, x)         SetEnumCSGlobalVar(i, j, x)
+  #define SetCSGlobalArrayVar(i, j, k, x) SetEnumCSGlobalArrayVar(i, j, k, x)
+  #define GetCSGlobalVar(i, j)            GetEnumCSGlobalVar(i, j)
+  #define GetCSGlobalArrayVar(i, j, k)    GetEnumCSGlobalArrayVar(i, j, k)
+
+  #define SetPtrVar(i, x)                 SetEnumPtrVar(i, x)
+  #define SetPtrArrayVar(i, j, x)         SetEnumPtrArrayVar(i, j, x)
+  #define GetPtrVar(i)                    GetEnumPtrVar(i)
+  #define GetPtrArrayVar(i, j)            GetEnumPtrArrayVar(i, j)
+
+#else
+
+  void SetEnumGlobalVar(enum globalP var, double data);
+  double GetEnumGlobalVar(enum globalP var);
+  void SetEnumGlobalArrayVar(enum globalParray var, unsigned index, double data);
+  double GetEnumGlobalArrayVar(enum globalParray var, unsigned index);
+  void SetEnumCSGlobalVar(enum csglobalQ var, unsigned cs, double data);
+  double GetEnumCSGlobalVar(enum csglobalQ var, unsigned cs);
+  void SetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs, double data);
+  double GetEnumCSGlobalArrayVar(enum csglobalQarray var, unsigned index, unsigned cs);
+  void SetEnumPtrVar(enum ptrM var, double data);
+  double GetEnumPtrVar(enum ptrM var);
+  void SetEnumPtrArrayVar(enum ptrMarray var, unsigned index, double data);
+  double GetEnumPtrArrayVar(enum ptrMarray var, unsigned index);
+
+  #define SetGlobalVar(i, x)              SetEnumGlobalVar(i, x)
+  #define SetGlobalArrayVar(i, j, x)      SetEnumGlobalArrayVar(i, j, x)
+  #define GetGlobalVar(i)                 GetEnumGlobalVar(i)
+  #define GetGlobalArrayVar(i, j)         GetEnumGlobalArrayVar(i, j)
+
+  #define SetCSGlobalVar(i, j, x)         SetEnumCSGlobalVar(i, j, x)
+  #define SetCSGlobalArrayVar(i, j, k, x) SetEnumCSGlobalArrayVar(i, j, k, x)
+  #define GetCSGlobalVar(i, j)            GetEnumCSGlobalVar(i, j)
+  #define GetCSGlobalArrayVar(i, j, k)    GetEnumCSGlobalArrayVar(i, j, k)
+
+  #define SetPtrVar(i, x)                 SetEnumPtrVar(i, x)
+  #define SetPtrArrayVar(i, j, x)         SetEnumPtrArrayVar(i, j, x)
+  #define GetPtrVar(i)                    GetEnumPtrVar(i)
+  #define GetPtrArrayVar(i, j)            GetEnumPtrArrayVar(i, j)
+
+#endif
+// end of #ifdef _EnumMode_
+#else
+// ***** Standard default mode *****
+#define PVT_BufferSize 8192
+#define PVT_WriteIdx 8193
+#define PVT_ReadIdx 8194
+#define PVT_Count 8195
+#define PVT_Scale 8196
+#define PVT_Time 8197
+#define PVT_Pos 8297
+#define PVT_Vel 8897
+#define target_pos1 9497
+#define target_pos2 9498
+#define target_pos3 9499
+#define target_pos4 9500
+#define target_pos5 9501
+#define target_move_time 9502
+#define target_accel_time 9503
+#define target_scurve_time 9504
+#define MyInit_Flag 9505
+#define IsNewData 9506
+#define base 9507
+#define Homing_State 9508
+#define Homing_Done 9509
+#define Homing_Timer 9510
+#define max_length 9511
+#define Last_Act_Pos 9512
+#define Last_Iq_Cmd 9513
+#define threshold_iqcmd 9514
+#define fe_max 9515
+#define Homing_Stop_Reason 9516
+#define error 9517
+#endif
+#endif
+#endif //_PP_PROJ_H_
